2cccc85ab4c965ed3837848ab36f1a71a80bec54
Jennifer Reif <jennifer.reif@neo4j.com>
2024-11-26 15:06:10 -0600

    Add CypherDSL, remove setters

--- src/main/java/com/example/appspringdata/Role.java
+++ src/main/java/com/example/appspringdata/Role.java
@@ -28,10 +28,6 @@ public class Role {
         return role;
     }
 
-    public void setRole(String role) {
-        this.role = role;
-    }
-
     public Person getPerson() {
         return person;
     }
